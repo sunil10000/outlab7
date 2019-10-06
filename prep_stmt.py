7b7a613d2986e5e2eebadc7fffad6a39edf10491
KaranAgarwalla <KaranAgarwalla>
2019-10-06 18:57:58 +0530

    Task8 Done

--- prep_stmt.py
+++ prep_stmt.py
@@ -30,12 +30,12 @@ def insert_match():
 	conn = sqlite3.connect('ipl.db')
 	crsr = conn.cursor()
 	crsr.execute("""INSERT INTO MATCH (match_id, season_year, team1, team2, battedfirst, battedsecond, venue_name, 
-                city_name, country_name, toss_winner, match_winner, toss_name, win_type, man_of_match, win_margin)
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""",
-                 (match_id, season_year, team1, team2, battedfirst,
-                 battedsecond, venue_name,city_name, country_name, toss_winner,
-                 match_winner, toss_name,
-                 win_type, man_of_match, win_margin))
+				city_name, country_name, toss_winner, match_winner, toss_name, win_type, man_of_match, win_margin)
+				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""",
+				(match_id, season_year, team1, team2, battedfirst,
+				battedsecond, venue_name,city_name, country_name, toss_winner,
+				match_winner, toss_name,
+				win_type, man_of_match, win_margin))
 	conn.commit()
 	conn.close()
 
@@ -49,8 +49,8 @@ def insert_player():
 	conn = sqlite3.connect('ipl.db')
 	crsr = conn.cursor()
 	crsr.execute("""INSERT INTO PLAYER (player_id, player_name, dob, batting_hand, 
-            bowling_skill, country_name) VALUES (?, ?, ?, ?, ?, ?);""",
-            (player_id, player_name, dob, batting_hand,bowling_skill, country_name))
+			bowling_skill, country_name) VALUES (?, ?, ?, ?, ?, ?);""",
+			(player_id, player_name, dob, batting_hand,bowling_skill, country_name))
 	conn.commit()
 	conn.close()
 
@@ -65,9 +65,9 @@ def insert_player_match():
 	team_id = int(input())
 	conn = sqlite3.connect('ipl.db')
 	crsr = conn.cursor()
-	crsr.executemany("""INSERT INTO PLAYER_MATCH (playermatch_key, match_id, player_id, batting_hand, bowling_skill, role_desc, 
-            team_id) VALUES (?, ?, ?, ?, ?, ?, ?);""",
-            (playermatch_key, match_id, player_id, batting_hand, bowling_skill, role_desc, team_id))
+	crsr.execute("""INSERT INTO PLAYER_MATCH (playermatch_key, match_id, player_id, batting_hand, bowling_skill, 
+			role_desc, team_id) VALUES (?, ?, ?, ?, ?, ?, ?);""",
+			(playermatch_key, match_id, player_id, batting_hand, bowling_skill, role_desc, team_id))
 	conn.commit()
 	conn.close()
 
@@ -86,24 +86,23 @@ def insert_ball():
 	conn = sqlite3.connect('ipl.db')
 	crsr = conn.cursor()
 	crsr.execute("""INSERT INTO BALL_BY_BALL (match_id, innings_no, over_id, ball_id, striker_batting_position, runs_scored, extra_runs,\
-    out_type, striker, non_striker, bowler) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""",
-	    (match_id, innings_no, over_id, ball_id,
-	    striker_batting_position, runs_scored, extra_runs,
-	    out_type, striker, non_striker, bowler))
+				out_type, striker, non_striker, bowler) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""",
+				(match_id, innings_no, over_id, ball_id,
+				striker_batting_position, runs_scored, extra_runs,
+				out_type, striker, non_striker, bowler))
 	conn.commit()
 	conn.close()
 
 
-
-if (table_no==1):
+if table_no == 1:
 	insert_team()
-elif (table_no==2):
+elif table_no == 2:
 	insert_player()
-elif (table_no==3):
+elif table_no == 3:
 	insert_match()
-elif (table_no==4):
+elif table_no == 4:
 	insert_player_match()
-elif (table_no==5):
+elif table_no == 5:
 	insert_ball()
 else:
 	print("wrong_input")
